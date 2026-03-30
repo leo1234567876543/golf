d02aab6646891cd6e64acf8d815bea1dab1a9a3c
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-30 22:37:39 +0200

    Refactor button handling for golf course selection

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ st.text("Wähle aus was du machen möchtest!")
 
 datei=open("aufzeichnung.txt", "a")
 st.write("🏌️⛳")
-st.write("Auf welche Goflplatz hast du gerade gespielt?")
+if st.button("Aufzeichnen"):
+    st.write("Auf welche Goflplatz hast du gerade gespielt?")
 
-if st.button("Golfplatz Gastein ⛳"):
-    st.write("ab")
+    if st.button("Golfplatz Gastein ⛳"):
+        st.write("ab")
 
-if st.button("Ein anderer Golfplatz ⛳"):
-    st.write("a")
+    if st.button("Ein anderer Golfplatz ⛳"):
+        st.write("a")
+if st.button("A"):
+    st.write("aaaa")
