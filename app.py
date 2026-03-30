bddbf30f034fde532cf56acc13a12612d950d0b9
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-30 21:53:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.title("Golf merker")
 st.text("Wähle aus was du machen möchtest!")
 
 if st.button("Aufzeichnung Platz"):
-    datei=open("aufzeichnung.txt", "r")
-    st.info(f"Du warst schon einmal in {datei}!")
+    datei=open("aufzeichnung.txt", "a")
+    st.info("Auf welche Goflplatz hast du gerade gespielt?")
