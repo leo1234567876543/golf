9641950e136ccfe589d3bd01bbb41c13d395f331
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-30 21:47:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import streamlit as st
 
 st.title("Golf merker")
 st.text("Wähle aus was du machen möchtest!")
+
+if st.button("Aufzeichnung Platz")
+    datei=open("aufzeichnung.txt", "a")
+    print(datei)
