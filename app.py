6ff370e4c9b959af366d3336eb60f6de65946615
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-30 23:00:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,12 @@ if st.button("Aufzeichnen"):
     
     if st.session_state.step >= 1:
         col1, col2 = st.columns(2)
-        if col1.button("Option A"):
+        if col1.button("Golfplatz Gastein):
             st.session_state.step = 2
+            ort = ("Golfplatz Gastein")
+           
         if col2.button("Option B"):
             st.session_state.step = 2
+            ort=  st.write_input("")
 
   
