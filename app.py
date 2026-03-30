fdd7d44fd4458915b533c0ef026004cdf83936fa
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-30 23:03:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ st.session_state.step = 0
 datei=open("aufzeichnung.txt", "a")
 st.write("🏌️⛳")
 if st.button("Aufzeichnen"):
-    st.session_state.step = 1
+    butt = 1
     st.write("Auf welche Goflplatz hast du gerade gespielt?")
     
-    if st.session_state.step >= 1:
-        col1, col2 = st.columns(2)
-        if col1.button("Golfplatz Gastein):
-            st.session_state.step = 2
+    if butt == 1:
+        
+        if st.button("Golfplatz Gastein):
+            
             ort = ("Golfplatz Gastein")
+            
+            
            
-        if col2.button("Option B"):
-            st.session_state.step = 2
+        if st.button("Option B"):
+           
             ort=  st.write_input("")
 
   
