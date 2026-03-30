3aa4e19cec9aaf8b3f892ab31f6c86ade7f87e5d
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-30 22:33:40 +0200

    Add buttons for golf course selection

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ st.text("Wähle aus was du machen möchtest!")
 datei=open("aufzeichnung.txt", "a")
 st.write("🏌️⛳")
 st.write("Auf welche Goflplatz hast du gerade gespielt?")
+
+if st.button("Golfplatz Gastein ⛳")
+
+if st.button("Ein anderer Golfplatz ⛳")
