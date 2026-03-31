860e17a9c6e905884a09e2bb8d934fb0cb35bf18
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-31 09:13:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,5 @@
-# golf_tracker_app.py
 import streamlit as st
 import os
 
-DATEI = "aufzeichnung.txt"
-
-
-def aufzeichnen(ort, datum, sterne):
-    with open(DATEI, "a") as datei:
-        datei.write(f"{ort},{datum},{sterne}\n")
-
-st.title("⛳ Golf")
-
-menu = st.sidebar.selectbox("Menü auswählen", ["Aufzeichnen", "Lesen"])
-
-if menu == "Aufzeichnen":
-    st.header("Neue Runde eintragen")
-    
-    ort_option = st.radio("Ort auswählen oder eigenen Ort eingeben", ["Gastein", "Anderer Ort"])
-    if ort_option == "Gastein":
-        ort = "Gastein"
-    else:
-        ort = st.text_input("Gib den Ort ein")
-    
-    datum = st.date_input("Datum der Runde")
-    sterne = st.slider("Bewertung (1=schlecht, 5=sehr gut)", 1, 5, 3)
-    
-    if st.button("Speichern"):
-        if ort:
-            aufzeichnen(ort, datum, sterne)
-            st.success(f"Runde in {ort} am {datum} mit {sterne} Stern(en) gespeichert!")
-        else:
-            st.error("Bitte Ort eingeben!")
-
-
+st.title("Golf 🏌️⛳")
+st.text("Wähle aus")
