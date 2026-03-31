7a653b250641e50eff42d095bd8e42f847b3a187
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-31 09:17:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 
 st.title("Golf 🏌️⛳")
 st.text("Wähle aus")
-menue=st.sidebar.selectbox("Wähl bitte zwischen ["Aufzeichnen", "Lesen"]
+menue=st.sidebar.selectbox("Wähl bitte zwischen", ["Aufzeichnen", "Lesen"])
