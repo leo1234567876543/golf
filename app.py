79c9f3f77b2b7ea768208660513bf1fb20179b66
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-31 09:33:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ if menue=="Aufzeichnen":
   if ort=="In einen anderen Ort":
         eintrag_ort=st.text_input("Gib hier den Ort ein")
   datum=st.date_input("Gib hier das Datum ein")
-  sterne=st.slider("Gib hier deine eigene Bewertung ein (1 Schlecht 10 Gut)", 1, 10, 3)
+  sterne=st.slider("Gib hier deine eigene Bewertung ein (1 Schlecht - 10 Gut)", 1, 10, 3)
+  gutes=st.text_input("Schreibe mind 1 gute sache vom Spiel auf")
