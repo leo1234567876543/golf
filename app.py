99a9c1aeec8b084c60e2f209d84fe37328154169
leo1234567876543 <leonhard.fleiss@protonmail.com>
2026-03-31 09:22:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import os
 st.title("Golf 🏌️⛳")
 st.text("Wähle aus")
 menue=st.sidebar.selectbox("Wähl bitte zwischen", ["Aufzeichnen", "Lesen"])
+
+if menue=="Aufzeichnen":
+  st.text("Neues Ereigniss eintragen")
+  ort=st.radio("Wähl hier bitte den Ort aus wo du gespielt hast", ["Gastein", "In einen anderen Ort"]
+  if ort=="Gastein":
+        eintrag_ort="Gastein"
+  if ort=="In einen anderen Ort":
+        eintrag_ort=st.text_input("Gib hier den Ort ein")
